1714f4ff8d67ca58d5b394b32c54849912a93586
mzvast <mzvast@gmail.com>
2019-07-10 19:49:26 +0800

    chore update doczrc

--- packages/galaco/doczrc.js
+++ packages/galaco/doczrc.js
@@ -5,8 +5,13 @@
  * @create date 2019-02-21 16:29:48
  */
 /* eslint-disable max-len,babel/new-cap,operator-linebreak,fecs-export-on-declare,space-before-function-paren */
+import pkg from './package.json';
+const libName = pkg.name;
 export default {
     typescript: true,
     wrapper: 'src/docs/wrapper',
-    codeSandbox: false
+    codeSandbox: false,
+    base: `/${libName}/`,
+    ordering: 'ascending',
+    description: pkg.description,
 };
